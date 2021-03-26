090f203fa28097f511db04c249dcfb332116909e
accidentlywoo <accidentlywoo@gmail.com>
2021-03-26 18:08:31 +0900

    feat : 모듈 구성해보기.패키지 설명 머선 129

--- hello/main/hello.go
+++ hello/main/hello.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 
-	"rsc.io/quote"
+	"example.com/greetings"
 )
 
 func main() {
 	// fmt.Println("Hello, World!")
-	fmt.Println(quote.Go())
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
